1e0d2ac980bc91a9621ecf6e95ad25356e2f0e7b
limbaniafagalde <limbufagalde@gmail.com>
2022-05-18 15:17:16 +0200

    Styling Services Section

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
 body{
     background: #1b1b1b;
+    font-family: "Inter", sans-serif;
 }
 
 button{
@@ -21,6 +22,7 @@ button{
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: "Inter", sans-serif;
     &:hover{
         background-color: #23d997;
         color: white;
@@ -66,4 +68,4 @@ p{
 
 `;
 
-export default GlobalStyle;
+export default GlobalStyle; 
