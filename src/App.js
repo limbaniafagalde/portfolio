87a295f03c64fec71ec3b7b5c2de9b002e2c8854
limbaniafagalde <limbufagalde@gmail.com>
2022-05-25 10:41:26 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@
 import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Import Components
 import Nav from "./components/Nav";
+//Router
+import {Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav/>
-      <AboutUs/>
+
+      <Routes>
+
+        <Route path="/" element={<AboutUs/>} />
+
+        <Route path="/work" element={<OurWork/>} />
+
+        <Route path="/contact" element={<ContactUs/>} />
+
+      </Routes>
+
     </div>
   );
 }
